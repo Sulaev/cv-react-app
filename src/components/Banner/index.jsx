e834ee9d1b5334cb0bdf59f951533e61ec91a0b7
Sulaev <sulaev2004@mail.ru>
2025-02-05 20:31:36 +0300

    Add new styles for Banner + children.

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,8 +2,15 @@ import "./styles.css"
 
 export const Banner = () => {
     return <div className="Banner">
-        <h1>egor sulaev</h1>
-        <h1>Your success is my priority</h1>
-        <p>During the past year, I have gained experience in various companies, where I have developed and maintained frontend interfaces for web applications. My expertise includes working with HTML, CSS, JavaScript, React.js, TypeScript, and Git, as well as participating in projects aimed at improving usability and performance.</p>
+        <div className="top-conteiner">
+            <h1 className="Text__header">egor sulaev</h1>
+            <h1 className="Text__header">{'<'} your success <br /> is my priority {'>'}</h1>
+        </div>
+        <div className="center-container">   
+            <p className="history-block Text__regular">during the past year, I have gained experience in various companies, where I have developed and maintained frontend interfaces for web applications. My expertise includes working with HTML, CSS, JavaScript, React.js, TypeScript, and Git, as well as participating in projects aimed at improving usability and performance.</p>
+        </div>
+        <div className="bottom-container"> 
+            <h1 className="Text__header">frontend developer</h1>
+        </div>
     </div>
 }
