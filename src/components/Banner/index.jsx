55adf06f8ef20a49f14d312adc3779d891e901ca
Sulaev <sulaev2004@mail.ru>
2025-02-08 00:06:11 +0300

    rename card style name

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,15 +3,15 @@ import { Avatar } from "@/components/Avatar"
 
 export const Banner = () => {
     return <div className="Banner">
-        <div className="top-conteiner">
+        <div className="TopConteiner">
             <h1 className="Text__header">egor sulaev</h1>
             <h1 className="Text__header">{'<'} your success <br /> is my priority {'>'}</h1>
         </div>
-        <div className="center-container">   
-            <p className="history-block Text__regular">during the past year, I have gained experience in various companies, where I have developed and maintained frontend interfaces for web applications. My expertise includes working with HTML, CSS, JavaScript, React.js, TypeScript, and Git, as well as participating in projects aimed at improving usability and performance.</p>
+        <div className="ContentContainer">
+            <p className="HistoryBlock Text__regular">during the past year, I have gained experience in various companies, where I have developed and maintained frontend interfaces for web applications. My expertise includes working with HTML, CSS, JavaScript, React.js, TypeScript, and Git, as well as participating in projects aimed at improving usability and performance.</p>
             <Avatar />
         </div>
-        <div className="bottom-container"> 
+        <div className="BottomContainer">
             <h1 className="Text__header">frontend developer</h1>
         </div>
     </div>
