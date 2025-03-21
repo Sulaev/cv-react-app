bc359e03d713a7b6070c75f94578e3067f604a65
Sulaev <sulaev2004@mail.ru>
2025-03-21 19:04:01 +0300

    bug research

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,28 @@ import { Main } from "./Pages/Main"
 // import "./styles.css"
 import "./i18n"
 import { useTranslation } from "react-i18next"
+import { useState, useEffect } from "react"
 
 function App() {
   const { t } = useTranslation()
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768)
+    }
+
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
+  if (isMobile) {
+    return (
+      <div>
+        <Main />
+      </div>
+    )
+  }
   return (
     <div style={{ height: "100%" }}>
       <Main />
